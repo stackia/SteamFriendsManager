1572c8c04e1d5cdc63b42c66b00bccad01597489
Stackia <jsq2627@gmail.com>
2015-01-04 00:42:36 +0800

    Auto check for new version on startup. Close #1.

--- SteamFriendsManager/ViewModel/MainWindowViewModel.cs
+++ SteamFriendsManager/ViewModel/MainWindowViewModel.cs
@@ -1,8 +1,14 @@
 using System;
 using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Reflection;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Threading;
+using MahApps.Metro.Controls.Dialogs;
+using Newtonsoft.Json;
 using SteamFriendsManager.Page;
 using SteamFriendsManager.Service;
 
@@ -10,6 +16,7 @@ namespace SteamFriendsManager.ViewModel
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private RelayCommand _checkForNewVersion;
         private object _currentPage;
         private RelayCommand _stopSteamService;
         private RelayCommand _switchBack;
@@ -95,6 +102,45 @@ namespace SteamFriendsManager.ViewModel
             {
                 return _stopSteamService ??
                        (_stopSteamService = new RelayCommand(async () => { await _steamClientService.StopAsync(); }));
+            }
+        }
+
+        public RelayCommand CheckForNewVersion
+        {
+            get
+            {
+                return _checkForNewVersion ?? (_checkForNewVersion = new RelayCommand(async () =>
+                {
+                    try
+                    {
+                        var request = WebRequest.Create("http://steamcn.com/sfm_version_metadata.json");
+                        var response = await request.GetResponseAsync();
+                        var responseStream = response.GetResponseStream();
+                        if (responseStream == null)
+                            return;
+                        using (var reader = new StreamReader(responseStream))
+                        {
+                            dynamic versionMetadata = JsonConvert.DeserializeObject(await reader.ReadToEndAsync());
+                            var version = Version.Parse(versionMetadata.LatestVersion.ToString());
+                            if (version > Assembly.GetExecutingAssembly().GetName().Version)
+                            {
+                                MessengerInstance.Send(new ShowMessageDialogMessageWithCallback("更新提示",
+                                    string.Format("现在有新版（v{0}）可用，是否要前往下载？", version as Version),
+                                    MessageDialogStyle.AffirmativeAndNegative,
+                                    result =>
+                                    {
+                                        if (result != MessageDialogResult.Affirmative)
+                                            return;
+
+                                        Process.Start(versionMetadata.DownloadUrl.ToString());
+                                    }));
+                            }
+                        }
+                    }
+                    catch (WebException)
+                    {
+                    }
+                }));
             }
         }
     }
